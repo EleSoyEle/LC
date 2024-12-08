b7d376053240653dfd0df5605ad9a4f41001bedf
EleSoyEle <angelgvsgm@gmail.com>
2024-12-08 17:48:47 -0600

    Añadiendo un matmul mas eficiente

--- kernel.cl
+++ kernel.cl
@@ -8,6 +8,34 @@ __kernel void DotP(
 
 }
 
+__kernel void MatMul(
+    __global float* mat1,
+    __global float* mat2,
+    __global float* matmul_re,
+    __const int s1fil,
+    __const int s1col,
+    __const int s2fil,
+    __const int s2col){
+    
+
+    int gid = get_global_id(0);
+    
+    int n = s1col; //Numero comun de filas y columnas
+    
+    int filas = s1fil;
+    int columnas = s2fil;
+    float s = 0;
+    int j = gid%columnas;
+    int i = (gid-j)/filas;
+    
+    
+    for(int k=0;k<n;k++){
+        s += mat1[(int)s1fil*i+k]*mat2[(int)s2fil*k+j];
+    }
+    
+    matmul_re[gid]=s;
+}
+
 __kernel void AddMat(
         __global float* mat1,
         __global float* mat2,
--- utils.c
+++ utils.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#define CL_TARGET_OPENCL_VERSION 300
+#include <CL/cl.h>
+#include <CL/opencl.h>
+#include <time.h>
 
 char* readTextFile(char filename[]){
     FILE* file = fopen(filename,"r");
@@ -57,4 +62,192 @@ float** oned2twod(float* array,int size,int s[]){
         }
     }
     return new_array;
+}
+
+//Se asume que los vectores tienen el mismo tamaño
+float dot_product(cl_program program,cl_command_queue queue,cl_context context,float* v1,float* v2,int s){
+    //Pasamos los dos vectores a la memoria del dispositivo
+    cl_mem buffer_v1 = clCreateBuffer(context,CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR,sizeof(float)*s,v1,NULL);
+    cl_mem buffer_v2 = clCreateBuffer(context,CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR,sizeof(float)*s,v2,NULL);
+    //Creamos el vector con las multiplicaciones
+    cl_mem mult_buffer = clCreateBuffer(context,CL_MEM_WRITE_ONLY,sizeof(float)*s,NULL,NULL);
+
+    cl_kernel kernel = clCreateKernel(program,"DotP",NULL);
+    clSetKernelArg(kernel,0,sizeof(cl_mem),(void*)&buffer_v1);
+    clSetKernelArg(kernel,1,sizeof(cl_mem),(void*)&buffer_v2);
+    clSetKernelArg(kernel,2,sizeof(cl_mem),(void*)&mult_buffer);
+
+    size_t global_size = s;
+    clEnqueueNDRangeKernel(queue,kernel,1,NULL,&global_size,NULL,0,NULL,NULL);
+    float* mult_ptr = (float*)calloc(s,sizeof(float));
+    clEnqueueReadBuffer(queue,mult_buffer,CL_TRUE,0,sizeof(float)*s,mult_ptr,0,NULL,NULL);
+
+    float dot_p = 0;
+    for(int i=0;i<s;i++){
+        dot_p += mult_ptr[i];
+    }
+    clReleaseMemObject(buffer_v1);
+    clReleaseMemObject(buffer_v2);
+    clReleaseMemObject(mult_buffer);
+    clReleaseKernel(kernel);
+    free(mult_ptr);
+    return dot_p;
+}
+
+
+//Calcula la matriz transpuesta
+float** Tmat(float** mat,int size[]){
+    float** mat_trasp = (float**)calloc(size[1],sizeof(float*));
+    for(int i=0;i<size[1];i++){
+        mat_trasp[i] = (float*)calloc(size[0],sizeof(float));
+        for(int j=0;j<size[0];j++){
+            mat_trasp[i][j] = mat[j][i];
+        }
+    }
+    return mat_trasp;
+}
+
+float** make_zero_mat(int dim[]){
+    float** mat_zero = (float**)calloc(dim[0],sizeof(float*));
+    for(int i=0;i<dim[0];i++){
+        float* vec_zero = (float*)calloc(dim[1],sizeof(float));
+        mat_zero[i]=vec_zero;
+    }
+    return mat_zero;
+}
+
+
+
+
+//s1:[m,k], s2:[j,n] ---> s1xs2:[m,n]
+float** matmul(cl_program program,cl_command_queue queue,cl_context context,float** mat1,float** mat2,int s1[],int s2[]){
+    float** matprod = (float**)calloc(s1[0],sizeof(float*));
+    float** mat_transp = Tmat(mat2,s2);
+    for(int i=0;i<s1[0];i++){
+        matprod[i] = (float*)calloc(s2[1],sizeof(float));
+        for(int j=0;j<s2[1];j++){
+            matprod[i][j] = dot_product(program,queue,context,mat1[i],mat_transp[j],s1[1]);
+        }
+    }
+    return matprod;
+}
+
+//Misma estructura pero el modo de calculo distinto
+cl_int kerr_mat = CL_SUCCESS;
+float** matmul_v2(cl_program program,cl_command_queue queue,cl_context context,float** mat1,float** mat2,int s1[],int s2[]){
+    //Puntero donde vamos a alojar los resultados al final
+    //float** matprod = make_zero_mat((float[2]){s1[1],s2[0]});
+    float* m1 = twod2oned(mat1,s1);
+    float* m2 = twod2oned(mat2,s2);
+    int dim_mat_s = s1[1]*s2[0];
+    cl_mem mat1_mem = clCreateBuffer(context,CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR,sizeof(float)*s1[0]*s1[1],m1,NULL);
+    cl_mem mat2_mem = clCreateBuffer(context,CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR,sizeof(float)*s2[0]*s2[1],m2,NULL);
+    cl_mem mat_mul = clCreateBuffer(context,CL_MEM_WRITE_ONLY,sizeof(float)*dim_mat_s,NULL,NULL);
+
+    cl_kernel kernel = clCreateKernel(program,"MatMul",&kerr_mat);
+
+    if(kerr_mat != CL_SUCCESS){
+        printf("Error al crear el kernel: %d \n",kerr_mat);
+    }
+    int s1fil = s1[0];
+    int s1col = s1[1];
+    int s2fil = s2[0];
+    int s2col = s2[1];
+
+    clSetKernelArg(kernel,0,sizeof(cl_mem),(void*)&mat1_mem);
+    clSetKernelArg(kernel,1,sizeof(cl_mem),(void*)&mat2_mem);
+    clSetKernelArg(kernel,2,sizeof(cl_mem),(void*)&mat_mul); 
+    clSetKernelArg(kernel,3,sizeof(cl_int),&s1fil);
+    clSetKernelArg(kernel,4,sizeof(cl_int),&s1col);
+    clSetKernelArg(kernel,5,sizeof(cl_int),&s2fil);
+    clSetKernelArg(kernel,6,sizeof(cl_int),&s2col);
+
+    size_t iters = dim_mat_s;
+    clEnqueueNDRangeKernel(queue,kernel,1,NULL,&iters,NULL,0,NULL,NULL);
+    float* mat_mu_ptr_arr = (float*)calloc(dim_mat_s,sizeof(float));
+    clEnqueueReadBuffer(queue,mat_mul,CL_TRUE,0,sizeof(float)*dim_mat_s,mat_mu_ptr_arr,0,NULL,NULL);
+    float** matmul_re = oned2twod(mat_mu_ptr_arr,dim_mat_s,(int[2]){s1[1],s2[0]});
+    
+    clReleaseKernel(kernel);
+    clReleaseMemObject(mat1_mem);
+    clReleaseMemObject(mat2_mem);
+    clReleaseMemObject(mat_mul);
+    free(mat_mu_ptr_arr);
+    free(m1);
+    free(m2);
+    return matmul_re;
+
+    
+}
+//Sin probar, seguramente no funciona
+//Ya funciona y fue probada, dejo el comentario anterior para recordarlo con amor
+cl_int kerr = CL_SUCCESS;
+float** add(cl_program program,cl_command_queue queue,cl_context context,float** mat1, float** mat2, int s1[],int s2[]){
+
+    float* m1 = twod2oned(mat1,s1);
+    float* m2 = twod2oned(mat2,s2);
+
+    cl_mem buff_mat1 = clCreateBuffer(context,CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR,sizeof(float)*s1[0]*s1[1],m1,NULL);
+    cl_mem buff_mat2 = clCreateBuffer(context,CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR,sizeof(float)*s2[0]*s2[1],m2,NULL);
+    cl_mem buff_madd = clCreateBuffer(context,CL_MEM_WRITE_ONLY,sizeof(float)*s1[0]*s2[1],NULL,NULL);
+    
+    cl_kernel kernel = clCreateKernel(program,"AddMat",&kerr);
+    if(kerr!=CL_SUCCESS){
+        printf("Error al crear el kernel:%d \n",kerr);
+    }
+    clSetKernelArg(kernel,0,sizeof(cl_mem),(void*)&buff_mat1);
+    clSetKernelArg(kernel,1,sizeof(cl_mem),(void*)&buff_mat2);
+    clSetKernelArg(kernel,2,sizeof(cl_mem),(void*)&buff_madd);
+    
+    size_t iters = s1[0]*s1[1];
+    clEnqueueNDRangeKernel(queue,kernel,1,NULL,&iters,NULL,0,NULL,NULL);
+    float* added_arr = (float*)calloc(s1[0],sizeof(float)*s1[0]*s1[1]);
+    clEnqueueReadBuffer(queue,buff_madd,CL_TRUE,0,sizeof(float)*s1[0]*s1[1],added_arr,0,NULL,NULL);
+    float** added_mat = oned2twod(added_arr,s1[0]*s1[1],s1);
+    
+    //Borramos las cosas
+    clReleaseKernel(kernel);
+    clReleaseMemObject(buff_mat1);
+    clReleaseMemObject(buff_mat2);
+    clReleaseMemObject(buff_madd);
+    free(added_arr);
+    free(m1);
+    free(m2);
+    return added_mat;
+
+}
+
+float** make_identity(int dim){
+    float** I = (float**)calloc(dim,sizeof(float*));
+    for(int i=0;i<dim;i++){
+        I[i] = (float*)calloc(dim,sizeof(float));
+        I[i][i]=1;
+    }
+    return I;
+}
+
+
+
+//0:Distribucion uniforme
+//1:Distribucion normal
+#define sqpi sqrt(2*CL_M_1_PI)
+#define PI 3.1415926535
+float** make_random_matrix(int dim[2],int mode){
+    float** rand_matrix = (float**)calloc(dim[0],sizeof(float*));
+    srand((unsigned int)time(NULL));
+    for(int i=0;i<dim[0];i++){
+        rand_matrix[i] = (float*)calloc(dim[1],sizeof(float));
+        for(int j=0;j<dim[1];j++){
+            float rv = (float)rand()/(float)(RAND_MAX);
+            
+            if(mode==1){
+                float rv2 = (float)rand()/(float)(RAND_MAX);
+                //Esta formula nos permite pasar de una distribucion uniforme a una normal
+                float z1 = sqrt(-2*log(rv))*cos(2*PI*rv2);
+                rv = z1;
+            }
+            rand_matrix[i][j] = rv;
+        }
+    }
+    return rand_matrix;
 }
